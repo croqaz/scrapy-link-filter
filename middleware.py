a116a537e355c80be1b0b2b6fd3048c2d1b65c49
Cristi Constantin <cristi.constantin@sent.com>
2019-04-13 14:25:45 +0100

    Implemented super basic filtering

--- middleware.py
+++ middleware.py
@@ -2,6 +2,7 @@ import logging
 
 from scrapy.http import Request
 from scrapy.exceptions import NotConfigured
+from scrapy.linkextractors import LinkExtractor
 
 logger = logging.getLogger(__name__)
 
@@ -19,3 +20,17 @@ class LinkFilterMiddleware:
         if not crawler.settings.getbool('LINK_FILTER_ENABLED'):
             raise NotConfigured
         return cls(crawler)
+
+    def process_spider_output(self, response, result, spider):
+        # Inspired from DepthMiddleware
+        def _filter(request):
+            if isinstance(request, Request) and response.meta.get('link_filtering'):
+                extractor = response.meta['link_filtering']
+                if isinstance(extractor, LinkExtractor):
+                    if not extractor.matches(request.url):
+                        logger.debug('Dropping link: %s', request.url, extra={'spider': spider})
+                        self.crawler.stats.inc_value('link_filtering/dropped_requests')
+                        return False
+            return True
+
+        return (r for r in result or () if _filter(r))
