0761b58db7f6c7dae0f70ad4f765f3735d41b6d4
Cristi Constantin <cristi.constantin@sent.com>
2019-05-21 19:30:38 +0100

    Updated LinkFilter to match case insensitive

--- scrapy_link_filter/middleware.py
+++ scrapy_link_filter/middleware.py
@@ -1,26 +1,39 @@
+import re
 import logging
-from typing import Optional
 from collections.abc import Iterable
 
 from scrapy.http import Request
 from scrapy.exceptions import IgnoreRequest
-from scrapy.linkextractors import LinkExtractor
+from scrapy.utils.misc import arg_to_iter
+from scrapy.utils.url import url_is_from_any_domain
 
 logger = logging.getLogger(__name__)
 
+SRE_Pattern = type(re.compile(''))
 
-def create_link_extractor(rules: str) -> Optional[LinkExtractor]:
-    """
-    Use the extraction rules defined per URL and return a new LinkExtractor.
-    Make sure the matching expressions are lower-case.
-    """
-    if not rules:
-        return
-    # Use these fields, ignore the rest
-    fields = ('allow', 'deny', 'allow_domains', 'deny_domains', 'restrict_text')
-    fixed_rules = {k: rules.get(k) for k in fields if isinstance(rules.get(k), Iterable)}
-    if fixed_rules:
-        return LinkExtractor(**fixed_rules)
+
+class LinkExtractor:
+
+    def __init__(self, allow=(), deny=(), allow_domains=(), deny_domains=()):
+
+        self.allow_res = [x if isinstance(x, SRE_Pattern) else re.compile(x, re.I) \
+            for x in arg_to_iter(allow)]
+        self.deny_res = [x if isinstance(x, SRE_Pattern) else re.compile(x, re.I) \
+            for x in arg_to_iter(deny)]
+
+        self.allow_domains = set(arg_to_iter(allow_domains))
+        self.deny_domains = set(arg_to_iter(deny_domains))
+
+    def matches(self, url):
+        if self.allow_domains and not url_is_from_any_domain(url, self.allow_domains):
+            return False
+        if self.deny_domains and url_is_from_any_domain(url, self.deny_domains):
+            return False
+
+        allowed = (regex.search(url) for regex in self.allow_res) if self.allow_res else [True]
+        denied = (regex.search(url) for regex in self.deny_res) if self.deny_res else []
+
+        return any(allowed) and not any(denied)
 
 
 class LinkFilterMiddleware:
@@ -64,6 +77,9 @@ class LinkFilterMiddleware:
         return (r for r in result or () if _filter(r))
 
     def _create_extractor(self, spider, request):
+        """
+        Use the extraction rules defined per URL and return a new LinkExtractor.
+        """
         rules = {}
         # Use the spider rules first
         if isinstance(getattr(spider, 'extract_rules', False), dict):
@@ -71,7 +87,13 @@ class LinkFilterMiddleware:
         # Update the spider rules with rules from the request
         if isinstance(request.meta.get('extract_rules'), dict):
             rules.update(request.meta['extract_rules'])
-        return create_link_extractor(rules)
+        if not rules:
+            return
+        # Use these fields, ignore the rest
+        fields = ('allow', 'deny', 'allow_domains', 'deny_domains')
+        fixed_rules = {k: rules.get(k) for k in fields if isinstance(rules.get(k), Iterable)}
+        if fixed_rules:
+            return LinkExtractor(**fixed_rules)
 
 
 DOWNLOADER_MIDDLEWARES = {
