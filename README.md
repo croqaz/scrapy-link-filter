0ff42e8a2dbb7f589d2fbf9fe89ff2ed96adcd5a
Cristi Constantin <cristi.constantin@sent.com>
2019-05-28 18:25:59 +0100

    Using the default LinkExtractor again
    Updated the docs and the setup config

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Scrapy-link-filter
 
-Spider Middleware that allows a Scrapy Spider to filter requests.
+![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg) [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+Spider Middleware that allows a [Scrapy Spider](https://scrapy.readthedocs.io/en/latest/topics/spiders.html) to filter requests.
 There is similar functionality in the [CrawlSpider](https://scrapy.readthedocs.io/en/latest/topics/spiders.html#crawlspider) already using Rules and in the [RobotsTxtMiddleware](https://scrapy.readthedocs.io/en/latest/topics/downloader-middleware.html#module-scrapy.downloadermiddlewares.robotstxt), but there are twists.
 This middleware allows defining rules dinamically per spider, or job, or request.
 
@@ -16,7 +18,7 @@ $ pip install git+https://github.com/croqaz/scrapy-link-filter
 
 ## Usage
 
-For the middleware to be enabled, it must be added in the project `settings.py`:
+For the middleware to be enabled as a Spider Middleware, it must be added in the project `settings.py`:
 
 ```
 SPIDER_MIDDLEWARES = {
@@ -26,7 +28,17 @@ SPIDER_MIDDLEWARES = {
 }
 ```
 
-Also, the rules must be defined either in the spider instance, in a `spider.extract_rules` dict, or per request, in `request.meta['extract_rules']`.
+Or, it can be enabled as a Downloader Middleware, in the project `settings.py`:
+
+```
+DOWNLOADER_MIDDLEWARES = {
+    # maybe other Downloader Middlewares ...
+    # can go before RobotsTxtMiddleware: 100
+    'scrapy_link_filter.middleware.LinkFilterMiddleware': 50,
+}
+```
+
+The rules must be defined either in the spider instance, in a `spider.extract_rules` dict, or per request, in `request.meta['extract_rules']`.
 Internally, the extract_rules dict is converted into a [LinkExtractor](https://docs.scrapy.org/en/latest/topics/link-extractors.html), which is used to match the requests.
 
 
--- scrapy_link_filter/middleware.py
+++ scrapy_link_filter/middleware.py
@@ -1,44 +1,16 @@
-import re
 import logging
-from collections.abc import Iterable
 
 from scrapy.http import Request
 from scrapy.exceptions import IgnoreRequest
-from scrapy.utils.misc import arg_to_iter
-from scrapy.utils.url import url_is_from_any_domain
+from scrapy.linkextractors import LinkExtractor
 
 logger = logging.getLogger(__name__)
-
-SRE_Pattern = type(re.compile(''))
-
-
-class LinkExtractor:
-
-    def __init__(self, allow=(), deny=(), allow_domains=(), deny_domains=()):
-
-        self.allow_res = [x if isinstance(x, SRE_Pattern) else re.compile(x, re.I) \
-            for x in arg_to_iter(allow)]
-        self.deny_res = [x if isinstance(x, SRE_Pattern) else re.compile(x, re.I) \
-            for x in arg_to_iter(deny)]
-
-        self.allow_domains = set(arg_to_iter(allow_domains))
-        self.deny_domains = set(arg_to_iter(deny_domains))
-
-    def matches(self, url):
-        if self.allow_domains and not url_is_from_any_domain(url, self.allow_domains):
-            return False
-        if self.deny_domains and url_is_from_any_domain(url, self.deny_domains):
-            return False
-
-        allowed = (regex.search(url) for regex in self.allow_res) if self.allow_res else [True]
-        denied = (regex.search(url) for regex in self.deny_res) if self.deny_res else []
-
-        return any(allowed) and not any(denied)
 
 
 class LinkFilterMiddleware:
     """
-    Spider Middleware that allows a Scrapy Spider to filter requests.
+    Downloader Middleware, or Spider Middleware,
+    that allows a Scrapy Spider to filter requests.
     """
 
     def __init__(self, crawler):
@@ -51,14 +23,14 @@ class LinkFilterMiddleware:
 
     def process_request(self, request, spider):
         """ Called as a Downloader Middleware """
-        if not isinstance(getattr(spider, 'extract_rules', False), dict):
+        extractor = self._create_extractor(spider, request)
+        if not extractor:
             return
 
-        extractor = self._create_extractor(spider, request)
-        if extractor and not extractor.matches(request.url.lower()):
+        if not extractor.matches(request.url):
             if self.debug:
-                logger.debug('Dropping link: %s', request.url, extra={'spider': spider})
-            self.crawler.stats.inc_value('link_filtering/dropped_requests')
+                logger.debug('Dropping request: %s', request, extra={'spider': spider})
+            self.crawler.stats.inc_value('link_filtering/d/dropped_requests')
             raise IgnoreRequest("Link doesn't match extract rules")
 
     def process_spider_output(self, response, result, spider):
@@ -66,11 +38,10 @@ class LinkFilterMiddleware:
         extractor = self._create_extractor(spider, response)
 
         def _filter(request):
-            if extractor and isinstance(request, Request) and \
-                    not extractor.matches(request.url.lower()):
+            if extractor and isinstance(request, Request) and not extractor.matches(request.url):
                 if self.debug:
-                    logger.debug('Dropping link: %s', request.url, extra={'spider': spider})
-                self.crawler.stats.inc_value('link_filtering/dropped_requests')
+                    logger.debug('Dropping request: %s', request, extra={'spider': spider})
+                self.crawler.stats.inc_value('link_filtering/s/dropped_requests')
                 return False
             return True
 
@@ -89,19 +60,15 @@ class LinkFilterMiddleware:
             rules.update(request.meta['extract_rules'])
         if not rules:
             return
-        # Use these fields, ignore the rest
-        fields = ('allow', 'deny', 'allow_domains', 'deny_domains')
-        fixed_rules = {k: rules.get(k) for k in fields if isinstance(rules.get(k), Iterable)}
-        if fixed_rules:
-            return LinkExtractor(**fixed_rules)
+        return LinkExtractor(**rules)
 
 
 DOWNLOADER_MIDDLEWARES = {
     # It goes before RobotsTxtMiddleware: 100
-    'scrapy_link_filter.middleware.LinkFilterMiddleware': 50,
+    'scrapy_link_filter.middleware.LinkFilterMiddleware': 50
 }
 
 SPIDER_MIDDLEWARES = {
     # It goes after DepthMiddleware: 900
-    'scrapy_link_filter.middleware.LinkFilterMiddleware': 950,
+    'scrapy_link_filter.middleware.LinkFilterMiddleware': 950
 }
